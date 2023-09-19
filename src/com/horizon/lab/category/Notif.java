06fac52663de3ef04e8db3fa2c3e0722744af0ad
ezio84 <brabus84@gmail.com>
2023-09-19 19:23:32 +0700

    HorizonLab: Less boring heads up option [2/2]
    Change-Id: Idbc0f8a971a29f18222034eeaacc309534236953
    Signed-offhby: ezio84 <brabus84@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- src/com/horizon/lab/category/Notif.java
+++ src/com/horizon/lab/category/Notif.java
@@ -66,6 +66,10 @@ public class Notif extends SettingsPreferenceFragment
 
         Settings.System.putIntForUser(resolver,
                  Settings.System.NOTIFICATION_GUTS_KILL_APP_BUTTON, 0, UserHandle.USER_CURRENT);
+        Settings.Global.putInt(resolver,
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LESS_BORING_HEADS_UP, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
