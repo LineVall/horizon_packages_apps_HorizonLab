f4a05f966baf90fbab3a364985647c1e64d0b2a9
Pranav Vashi <neobuddy89@gmail.com>
2025-04-22 21:32:12 +0700

    HorizonLab: Hide power menu on secure lockscreen [2/2]
    Change-Id: I110c9b7943d58707fd2f43d9c1fef5ac5b3660ef
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- src/com/horizon/lab/category/Lock.java
+++ src/com/horizon/lab/category/Lock.java
@@ -59,7 +59,14 @@ public class Lock extends SettingsPreferenceFragment
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
-    }  
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        Settings.System.putIntForUser(resolver,
+                 Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT);
+    }
 
     @Override
     public int getMetricsCategory() {
